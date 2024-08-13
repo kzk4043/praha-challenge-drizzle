471b1a0507bb27d0b9b28ae058ac008f70e42292
kzk4043 <okazakishoma@team-lab.com>
2024-08-13 09:23:44 +0900

    feat: update db

--- index.ts
+++ index.ts
@@ -1,15 +1,9 @@
-import { db } from './db/db';
-import { todos } from './db/schema';
+import { db } from "./db/db";
+import { todos } from "./db/schema";
 
 async function main() {
-  const result = db
-    .insert(todos)
-    .values({ name: 'Learn Drizzle', isCompleted: false })
-    .run();
-  console.log('result', result);
-
   const allTodo = db.select().from(todos).all();
-  console.log('allTodo', allTodo);
+  console.log("allTodo", allTodo);
 }
 
 main();
