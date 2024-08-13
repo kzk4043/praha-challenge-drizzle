1b06beac31723ea424e8000c6123bda7f3657389
kzk4043 <okazakishoma@team-lab.com>
2024-08-13 09:26:53 +0900

    fea: type

--- index.ts
+++ index.ts
@@ -1,9 +1,20 @@
 import { db } from "./db/db";
 import { todos } from "./db/schema";
+import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+
+type InsertTodo = InferInsertModel<typeof todos>;
+type Todo = InferSelectModel<typeof todos>;
+
+const insertTodo = (todo: InsertTodo) => {
+  return db.insert(todos).values(todo).run();
+};
 
 async function main() {
-  const allTodo = db.select().from(todos).all();
-  console.log("allTodo", allTodo);
+  const result = insertTodo({ name: "Learn TypeScript", isCompleted: false });
+  console.log("result", result);
+
+  const allTodo: Todo[] = db.select().from(todos).all();
+  console.log(allTodo);
 }
 
 main();
